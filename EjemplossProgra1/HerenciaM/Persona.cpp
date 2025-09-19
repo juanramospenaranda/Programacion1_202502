57ae529c919d618ed28c27dee00ed8da0439d3eb
juanramospenaranda <juanramospenaranda@una.cr>
2025-09-19 17:47:28 -0600

    Clase Persona completa

---
+++ EjemplossProgra1/HerenciaM/Persona.cpp
@@ -0,0 +1,18 @@
+#include "Persona.h"
+using namespace std;
+#include <iostream>
+
+
+	string Nombre;
+	string Cedula;
+	int Edad;
+
+	
+
+	void Persona::Mostrar()
+	{
+		cout << "Nombre:" << Nombre;
+	}
+	
+
+
---
+++ EjemplossProgra1/HerenciaM/Persona.h
@@ -0,0 +1,17 @@
+#pragma once
+
+using namespace std;
+#include <iostream>
+class Persona
+{
+public:
+	string Nombre;
+	string Cedula;
+	int Edad;
+
+
+
+	void Mostrar();
+	
+};
+
